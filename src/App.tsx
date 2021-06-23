0bb64e8c823d43e521c4bbea229b0f3b87ac11c8
Slawomir Kaczynski <slawomir.kaczynski@billennium.com>
2021-06-23 14:30:53 +0200

    Add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import * as tt from "@tomtom-international/web-sdk-maps";
+import { useEffect, useRef, useState } from "react";
 
-const App = () => {
-  return <div className="App"></div>;
+interface Props {
+}
+
+const App: React.FC<Props> = () => {
+  const mapElement: any = useRef()
+  const [map, setMap] = useState({})
+
+useEffect(() => {
+ let map = tt.map({
+  key: 'bJ07pLsJ2koMcN1GDHUu1hfSmGmg2dkE',
+  container: mapElement.current,
+});
+
+setMap(map)
+}, [])
+
+  return <div className="App">
+    <div ref={mapElement}></div>
+  </div>;
 };
 
 export default App;
